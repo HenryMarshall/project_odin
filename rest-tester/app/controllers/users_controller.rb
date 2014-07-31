1fe93b2e2a0308a03d5d63b2e4a4dd3ea1009b83
hbaughman <hbaughman741@gmail.com>
2014-07-31 16:41:26 -0400

    Create users controller; update routes

---
+++ rest-tester/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def index
+    @index = "The Index Action"
+  end
+
+  def edit
+    @edit = "The Edit Action"
+  end
+
+  def show
+    @show = "The Show Action"
+  end
+
+  def new
+    @new = "The New Action"
+  end
+
+end
