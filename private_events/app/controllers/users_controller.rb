334c793bdb5a794586fb4a95fec7b7f3036d4b2b
hbaughman <hbaughman741@gmail.com>
2014-08-27 13:09:10 -0400

    Create user new, create, show and corresponding views

--- private_events/app/controllers/users_controller.rb
+++ private_events/app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(username: params[:user][:username])
+    if @user.save
+      flash[:success] = "Created account"
+      redirect_to user_path(@user)
+    else
+      flash.now[:error] = "Could not save" 
+      render 'new'
+    end
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 end
