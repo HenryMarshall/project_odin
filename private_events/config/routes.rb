6103dba015c2deda297f2aec0c15d76b2f7b9a43
hbaughman <hbaughman741@gmail.com>
2014-08-27 13:13:02 -0400

    Create signin/signout routes

--- private_events/config/routes.rb
+++ private_events/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:create, :new, :show]
+  
+  get '/signin',  to: 'sessions#new',     via: 'get'
+  get '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
