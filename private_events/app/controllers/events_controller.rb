063ca66ca21b6afb9c92b3a87a8c59e366ef7b91
hbaughman <hbaughman741@gmail.com>
2014-08-27 20:44:10 -0400

    Create event routes and controller methods

---
+++ private_events/app/controllers/events_controller.rb
@@ -0,0 +1,15 @@
+class EventsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+end
--- private_events/config/routes.rb
+++ private_events/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users,       only: [:new, :create, :show]
   resources :sessions,    only: [:new, :create, :destroy]
+  resources :events,      only: [:new, :create, :show]
 
   get '/signup',  to: 'users#new',        via: 'get'
   get '/signin',  to: 'sessions#new',     via: 'get'
